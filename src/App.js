84e64f3d66e228039aad8c601e2f57ac2a80f7b9
lowlyHero <e.nunn1009@gmail.com>
2019-11-04 15:30:55 -0500

    Added node-sass to dependencies. Working on stretch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
-import "./App.css";
+import "./App.scss";
 import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
@@ -14,9 +13,6 @@ function App() {
         <div className="topRow">
           <div className="home">
             <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
             <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
@@ -29,7 +25,6 @@ function App() {
       </section>
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button onClick={() => setHomeScore(homeScore + 7)} className="homeButtons__touchdown">Home Touchdown</button>
           <button onClick={() => setHomeScore(homeScore + 3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
